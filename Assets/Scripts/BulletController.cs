8d4ad269027e8671b95b26610ee140b2012e9097
AliAlaei <aainsrm@gmail.com>
2023-03-30 15:10:03 -0700

    added more platforms + bullet now destroys after colliding with Player or Enemy

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    
+    public float lifetime = 2f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime); // destroy bullet after lifetime seconds
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") ||
+            collision.gameObject.CompareTag("Enemy"))
+        {
+            
+            Destroy(gameObject); // destroy bullet on collision
+        }
+    }
+    
+
+}
