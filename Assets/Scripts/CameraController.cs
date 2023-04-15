368666d7e6fe74f6de3a1bb17542b6219d23777d
ali-alaei <aainsrm@gmail.com>
2023-04-14 23:53:12 -0700

    Camera moves as player goes up

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float yOffset = 5f;
+    [SerializeField] private float moveSpeed = 5f;
+    private bool isMoving = false;
+
+
+    private void OnEnable()
+    {
+        Actions.OnEnemyKilled += MoveCamera;
+    }
+
+    private void OnDisable()
+    {
+        Actions.OnEnemyKilled -= MoveCamera;
+    }
+
+    public void MoveCamera()
+    {
+        if (!isMoving)
+        {
+            StartCoroutine(MoveCameraCoroutine());
+        }
+    }
+
+    private IEnumerator MoveCameraCoroutine()
+    {
+        isMoving = true;
+
+        Vector3 startPos = transform.position;
+        Vector3 endPos = transform.position + Vector3.up * yOffset;
+
+        float startTime = Time.time;
+        float journeyLength = Vector3.Distance(startPos, endPos);
+
+        while (transform.position != endPos)
+        {
+            float distCovered = (Time.time - startTime) * moveSpeed;
+            float journeyFraction = distCovered / journeyLength;
+
+            transform.position = Vector3.Lerp(startPos, endPos, journeyFraction);
+
+            yield return null;
+        }
+
+        isMoving = false;
+    }
+}
+
