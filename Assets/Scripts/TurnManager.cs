fa2940c9f138165728c628a3c516561df4cc2bd9
ali-alaei <aainsrm@gmail.com>
2023-04-29 03:36:11 -0700

    New stairs & enemies added

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -44,7 +44,7 @@ public class TurnManager : MonoBehaviour
         while (true)
         {
 
-            yield return new WaitForSeconds(4f);
+            yield return new WaitForSeconds(2f);
 
             // switching to enemy
             if (PlayerController.hasShot && !EnemyCollisionController.isDead)
